70bb9a70a0cf403c271a8200d606c0901795121a
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-02 12:15:31 -0700

    create function that return sibling to given node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,8 +11,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left->n == node->n)
-		return (node->parent->right);
-	else
+	if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
+
+	if (node->parent->right->n == node->n)
 		return (node->parent->left);
+	else
+		return (node->parent->right);
 }
