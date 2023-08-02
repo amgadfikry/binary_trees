5b0418b6601ea3c8824ff4343440f671909211a8
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-02 09:28:34 -0700

    create function that check if binary tree if prefect r not

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,14 +16,35 @@ int b_height(const binary_tree_t *tree)
 	if (tree->right == NULL && tree->left == NULL)
 		return (0);
 
-	if (tree->left)
-		lt = b_height(tree->left);
-	if (tree->right)
-		rt = b_height(tree->right);
+	lt = b_height(tree->left);
+	rt = b_height(tree->right);
+
 	if (lt > rt)
 		return (1 + lt);
 	else
 		return (1 + rt);
+}
+
+/**
+ * tree_balance - measure balnace factor of binary tree
+ * @tree: pointer to root of binary tree
+ * Return: 0 if same or positive number if left more than right
+ * or negative if right more than left
+ */
+
+int tree_balance(const binary_tree_t *tree)
+{
+	int lt = 0, rt = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		lt = 1 + b_height(tree->left);
+	if (tree->right)
+		rt = 1 + b_height(tree->right);
+
+	return (lt - rt);
 }
 
 /**
@@ -64,7 +85,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (is_full(tree))
 	{
-		if (b_height(tree->left) - b_height(tree->right) == 0)
+		if (tree_balance(tree) == 0)
 			return (1);
 		else
 			return (0);
