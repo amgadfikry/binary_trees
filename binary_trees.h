11d26ef7d7314977166e4f9dc051bf6455e3141c
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-01 18:27:54 -0700

    add prototype of function that print binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -30,6 +30,9 @@ typedef struct binary_tree_s avl_t;
 /*Max Binary Heap type def*/
 typedef struct binary_tree_s heap_t;
 
+/*prototype of function that print binary tree*/
+void binary_tree_print(const binary_tree_t *);
+
 /*mandatory tasks functions prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
