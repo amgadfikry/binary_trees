1560c1351bbb2526c6d563ab3695c94618997662
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-02 05:02:55 -0700

    create function that delete entire binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete entire binary tree
+ * @tree: pointer to root node of tree
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
+
+	free(tree);
+}
