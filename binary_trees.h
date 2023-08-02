ecefd2856e11e40ebc463eef701f14431f2d137f
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-01 18:22:05 -0700

    add standard libarary to header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
